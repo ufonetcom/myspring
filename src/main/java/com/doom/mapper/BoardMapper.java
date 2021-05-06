083a88911a7e032729dd2a7dd3bd2d24391e8548
ufonetcom <ufozx@naver.com>
2021-05-06 18:07:05 +0900

    mapper xml 테스트 중.. CRUD구현 중..

--- src/main/java/com/doom/mapper/BoardMapper.java
+++ src/main/java/com/doom/mapper/BoardMapper.java
@@ -1,13 +1,22 @@
 package com.doom.mapper;
 
 import com.doom.domain.BoardVO;
-import org.apache.ibatis.annotations.Select;
-import org.springframework.stereotype.Component;
 
 import java.util.List;
 
 public interface BoardMapper {
 
-    @Select("select * from tbl_board where board_no > 0")
+    //@Select("select * from tbl_board where board_no > 0")
     public List<BoardVO> getList();
+
+    public void insertBoard(BoardVO boardVO);
+
+    public void insertBoardSelectKey(BoardVO boardVO);
+
+    public BoardVO readBoard(Long board_no);
+
+    public int readBoardTotalCount();
+
+    public int deleteBoard(Long board_no);
+
 }
--- src/test/java/com/doom/mapper/BoardMapperTests.java
+++ src/test/java/com/doom/mapper/BoardMapperTests.java
@@ -1,6 +1,6 @@
 package com.doom.mapper;
 
-import junit.framework.TestCase;
+import com.doom.domain.BoardVO;
 import lombok.Setter;
 import lombok.extern.log4j.Log4j2;
 import org.junit.Test;
@@ -8,7 +8,6 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = {com.doom.config.RootConfig.class})
@@ -22,5 +21,46 @@ public class BoardMapperTests {
     public void testGetList() {
         mapper.getList().forEach(boardVO -> log.info(boardVO));
     }
+
+    @Test
+    public void testInsertBoard() {
+        BoardVO boardVO = new BoardVO();
+        boardVO.setTitle("mapper test");
+        boardVO.setContent("새로 작성하는 글");
+        boardVO.setWriter("doompoknew");
+
+        mapper.insertBoard(boardVO);
+
+        log.info(boardVO);
+    }
+
+    //insert하려는 board_no(PK)값을 mapper.xml에 selectkey 쿼리에 boardvo객체로 바인딩 된다.
+    //insert문이 실행되고 나서 추가된 해당 키값을 boardvo객체에 매핑해준다.
+    @Test
+    public void testInsertBoardSelectKey() {
+        BoardVO boardVO = new BoardVO();
+        boardVO.setTitle("mapper test sk");
+        boardVO.setContent("새로 작성하는 글 sk");
+        boardVO.setWriter("selectkey board insert");
+
+        mapper.insertBoardSelectKey(boardVO);
+
+        log.info(boardVO);
+    }
+
+    @Test
+    public void testReadBoard() {
+        //board테이블에서의 board_no값으로 테스트 한다.
+        BoardVO boardVO = mapper.readBoard(21L);
+
+        log.info(boardVO);
+    }
+
+    @Test
+    public void testDeleteBoard() {
+        log.info("Delete =>>>> {}", mapper.deleteBoard(21L));
+        log.info(mapper.readBoard(21L));
+    }
+
 
 }
